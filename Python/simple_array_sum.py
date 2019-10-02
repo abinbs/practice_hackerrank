a2e1490164cc23f92079d492b112dfae4524abb8
Sreejith-Murali <sreejithmurali1999@ieee.org>
2019-10-02 16:30:51 +0530

    simply added some comments

--- Python/simple_array_sum.py
+++ Python/simple_array_sum.py
@@ -1,13 +1,13 @@
 #!/bin/python3
 
-import os
+import os        #importing preprocessor directives
 import sys
 
 
-def simpleArraySum(ar):
+def simpleArraySum(ar):    
     sum=0
     for i in range(0,ar_count):
-        sum=sum + ar[i]
+        sum=sum + ar[i]   #finding sum
     return sum
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
