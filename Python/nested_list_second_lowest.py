702da87a7e3256b3d2f25bfb5ca0743520bf71dc
JibinJoseph2000 <49112780+JibinJoseph2000@users.noreply.github.com>
2019-10-03 19:22:01 +0400

    Updated

--- Python/nested_list_second_lowest.py
+++ Python/nested_list_second_lowest.py
@@ -1,16 +1,16 @@
-marksheet=[]
-scorelist=[]
+marksheet=[] # creates a list marksheet
+scorelist=[] # creates a list scorelist
 
 if __name__ == '__main__':
     for _ in range(int(input())):
-        name = input()
-        score = float(input())
+        name = input("Entre the name:")
+        score = float(input("Enter the score:"))
         marksheet+=[[name,score]]
         scorelist+=[score]
 
 
     scorelist = list(dict.fromkeys(scorelist))
-    b=sorted(scorelist)[1] 
+    b=sorted(scorelist)[1] # Function which sorts the list in the increasing order of scores  
 
     for a,c in sorted(marksheet):
         if c==b:
